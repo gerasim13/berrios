9cac7f10a2b4e7be698e53cc3d823538cbce8847
Павел Литвиненко <digitaldistortion@ya.ru>
2019-11-01 17:59:45 +0300

    simple bot

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 import './app.css';
 import ReactImage from './react.png';
+import { Form, Button, Image } from 'react-bootstrap';
+import axios from 'axios';
 
 export default class App extends Component {
-  state = { username: null };
+  state = { value: null };
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit = event => {
+    const { value } = this.state;
+    axios.post('/api/sendMessage', { message: value });
+    event.preventDefault();
   }
 
   render() {
-    const { username } = this.state;
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+      <div className='App'>
+        <header className="App-header">
+          <Image src={ReactImage} />
+        </header>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="text"
+              placeholder="Write me a few word"
+              value={this.state.value}
+              onChange={this.handleChange} />
+          </Form.Group>
+          <Button variant="primary" type="submit">Submit</Button>
+        </Form>
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const os = require('os');
-
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
 const app = express();
 
+const token = '1041346658:AAESWqCoTXPcfJtMRGBozrA19kNfdp_BlxE';
+const chat = '-362608791';
+const bot = new TelegramBot(token, { polling: true });
+
+function postMessage(chatId, message) {
+    console.log(chatId);
+    bot.sendMessage(chatId, message);
+}
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    postMessage(chatId, 'Received your message');
+});
+
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use(bodyParser.json());
+app.post('/api/sendMessage', (req, res) =>
+    postMessage(chat, req.body['message'])
+);
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
